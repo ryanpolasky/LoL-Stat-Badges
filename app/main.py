e1d2b37915349b68658281a840c467221165d6ad
ryanpolasky <ryanpolasky@hotmail.com>
2024-12-09 15:09:14 -0600

    Redirect base route to repo

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 from app.routers import badge
 import logging
 import os
@@ -18,11 +19,17 @@ app.include_router(badge.router, prefix="/badge", tags=["Badge"])
 # Mount the assets folder
 assets_path = os.path.join(os.path.dirname(__file__), "assets")
 app.mount("/assets", StaticFiles(directory=assets_path), name="assets")
+
 @app.get("/")
 async def root():
     logger.info("Root GET route accessed.")
-    return {"message": "Thanks for using my API! :)"}
+    # Return a thank you message and redirect to GitHub repo
+    response = {"message": "Thanks for using my API! :)"}
+    # Redirect to GitHub repo
+    return RedirectResponse(url="https://github.com/ryanpolasky/LoL-Stat-Badges", status_code=303)
 
 @app.head("/")
 async def head_root():
     logger.info("Root HEAD route accessed.")
+    # HEAD request does not have a body, just headers
+    return RedirectResponse(url="https://github.com/ryanpolasky/LoL-Stat-Badges", status_code=303)
